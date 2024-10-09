b03dc120a2cda9410e0d72dcd64c084fa017a39d
Wojciech Trapkowski <wojciechtrapkowski1@gmail.com>
2024-10-10 00:09:07 +0200

    switch to 32-bits

--- boot_sect_main.asm
+++ boot_sect_main.asm
@@ -1,36 +1,36 @@
 [org 0x7c00] ; tell the assembler that our offset is bootsector code
 
 mov bx, 0x9000 ; es:bx = 0x0000:0x9000 = 0x09000
-mov dh, 2 ; read 2 sectors
-call disk_load
+mov sp, bp
 
-mov dx, [0x9000] ; first word from first loaded sector, 0xdada
-call print_hex
+mov bx, MSG_REAL_MODE
+call print ; This will be written after the BIOS messages
 
-call print_nl
+call switch_to_pm
+jmp $ ; this will actually never be executed
 
-mov dx, [0x9000 + 512] ; first word from second loaded sector, 0xface
-call print_hex
 
-; that's it! we can hang now
-jmp $
-
-; remember to include subroutines below the hang
 %include "boot_sect_print.asm"
-%include "boot_sect_print_hex.asm"
-%include "boot_sect_disk.asm"
+; %include "boot_sect_print_hex.asm"
+; %include "boot_sect_disk.asm"
 
-; data
-HELLO:
-    db 'Hello, World', 0
+%include "32bit_gdt.asm"
+%include "32bit_print.asm"
+%include "32bit_switch.asm"
 
-GOODBYE:
-    db 'Goodbye', 0
+[bits 32]
+BEGIN_PM: ; after the switch we will get here
+    mov ebx, MSG_PROT_MODE
+    call print_string_pm
+    jmp $
 
-; padding and magic number
+MSG_REAL_MODE db "Started in 16-bit real mode", 0
+MSG_PROT_MODE db "Loaded 32-bit protected mode", 0
+
+; bootsector 0 
 times 510-($-$$) db 0
 dw 0xaa55
 
-; boot sector 1
-times 256 dw 0xdada ; boot sector 2
-times 256 dw 0xface ; boot sector 3
+; ; boot sector 1
+; times 256 dw 0xdada ; boot sector 2
+; times 256 dw 0xface ; boot sector 3
