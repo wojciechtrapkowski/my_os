72cf859f63117f25d17e0f1c2319ca12a07027ae
Wojciech Trapkowski <wojciechtrapkowski1@gmail.com>
2024-10-26 17:02:17 +0200

    add handling scrolling for screen output

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,4 +1,5 @@
 #include "../drivers/screen.h"
+#include "util.h"
 
 /* This will force us to create a kernel entry function instead of jumping to kernel.c:0x00 */
 void dummy_test_entrypoint() {
@@ -17,10 +18,17 @@ void test_whole_screen() {
 }
 
 void main() {
+
     clear_screen();
-    kprint_at("X", 1, 6);
-    kprint_at("This text spans multiple lines", 75, 10);
-    kprint_at("There is a line\nbreak", 0, 20);
-    kprint("There is a line\nbreak");
-    kprint_at("What happens when we run out of space?", 45, 24);
+
+    /* Fill up the screen */
+    int i = 0;
+    for (i = 0; i < 24; i++) {
+        char str[255];
+        int_to_ascii(i, str);
+        kprint_at(str, 0, i);
+    }
+
+    kprint_at("This text forces the kernel to scroll. Row 0 will disappear. ", 60, 24);
+    kprint("And with this text, the kernel will scroll again, and row 1 will disappear too!");
 }
